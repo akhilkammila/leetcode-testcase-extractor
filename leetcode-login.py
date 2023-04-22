8a92e342d591f564f1120b650fddaf3f6de64b5f
Akhil Kammila <akhilkammila@gmail.com>
2023-04-21 21:20:13 -0400

    logs into leetcode

--- leetcode-login.py
+++ leetcode-login.py
@@ -1,16 +1,29 @@
 import time
 import random
+import selenium
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 
+# get hidden auth variables
+from auth import gmail
+from auth import pswd
+
 options = webdriver.ChromeOptions()
-options.add_experimental_option("detach", True) #keeps window open after running for debugging
+# options.add_experimental_option("detach", True)
 driver = webdriver.Chrome(options=options)
 
-leetcode_link = "https://leetcode.com/"
+leetcode_login_link = "https://leetcode.com/accounts/login/"
 
 def sign_in():
-    driver.get(leetcode_link)
+    driver.get(leetcode_login_link)
+    driver.find_element(By.NAME, "login").send_keys(gmail)
+    driver.find_element(By.NAME, "password").send_keys(pswd)
+    driver.find_element(By.ID, "signin_btn").click()
+
+
+def wait():
+    a = input()
 
 if __name__ == "__main__":
     sign_in()
+    wait()
